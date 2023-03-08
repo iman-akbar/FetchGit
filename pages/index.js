10e4baa2472667912103447dca8867dc503ee181
iman.akbar <iman.akbar@mncgroup.comv>
2023-03-08 22:43:56 +0700

    add button

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,53 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from '@/styles/Home.module.css'
-import { Card, Grid, Text} from "@nextui-org/react";
+import { Card, Grid, Text, Button } from "@nextui-org/react";
 
 const Home = () => {
   const [allDataList, setAllDataList] = useState([])
   useEffect(() => {
     const loadData = async () => {
-     axios.get("https://api.github.com/users/iman-akbar/repos")
-       .then(( response ) => {
+      axios.get("https://api.github.com/users/iman-akbar/repos")
+        .then((response) => {
           console.log(response);
-            setAllDataList(response.data)
+          setAllDataList(response.data)
         })
     }
     loadData();
   }, [])
 
-  
-  return (      
-    <div> 
-      
+
+  return (
+    <div>
+
       <Grid.Container gap={3}>
-      {allDataList.map((item,index) => (
-        <Grid xs={6} sm={3} key={index}>
-          <Card
-            onClick={() => {
-              window.open(item.html_url)
-            }}
-            isPressable >
-            <Card.Body css={{ p: 50 }}>              
-              <Text css={{textAlign: 'center'}}>{item.name}</Text>
-            </Card.Body>
-             <Card.Divider />
-            <Card.Footer css={{justifyContent:"center"}}>                            
-                <Text css={{fontWeight: "$semibold", fontSize: "$sm"}}>
+        {allDataList.map((item, index) => (
+          <Grid xs={6} sm={3} key={index}>
+            <Card
+            >
+              <Card.Header>
+                <Text css={{ textAlign: 'center', fontWeight: "$bold", fontSize: "$xl" }}>{item.name}</Text>
+              </Card.Header>
+              <Card.Body css={{}}>
+                <Text css={{ fontWeight: "$semibold", fontSize: "$sm", textAlign: 'center' }}>
                   {item.html_url}
-                </Text>             
-            </Card.Footer>
-          </Card>
-        </Grid>
-      ))}
-      </Grid.Container>           
-      </div>
-    );
-  }
+                </Text>
+              </Card.Body>
+              <Card.Divider />
+              <Card.Footer css={{ justifyContent: "center" }}>
+                <Button size="sm" color="gradient" onClick={() => {
+                  window.open(item.html_url)
+                }}>
+                  Go To Link
+                </Button>                
+              </Card.Footer>
+            </Card>
+          </Grid>
+        ))}
+      </Grid.Container>
+    </div>
+  );
+}
 
 
 export default Home
