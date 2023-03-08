047027db8cd334f2bd0d29fc7e7334ff47b35a02
iman.akbar <iman.akbar@mncgroup.comv>
2023-03-08 17:17:00 +0700

    using useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,35 @@
-import { Inter } from 'next/font/google'
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from '@/styles/Home.module.css'
 
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allDataList: [],
-    };
-  }
-  componentDidMount() {
-    axios.get(`https://api.github.com/users/iman-akbar/repos`).then((res) => {
-      const allDataList = res.data.map((obj) => ({
-        title: obj.name,
-        url: obj.html_url,
-      }));
-      this.setState({ allDataList });
-      console.log(res);
-    });
-  }
-  render() {
-    return (
-      <div>
-        {this.state.allDataList.map((dataList, index) => {
+const Home = () => {
+  const [allDataList, setAllDataList] = useState([])
+  useEffect(() => {
+    const loadData = async () => {
+     axios.get("https://api.github.com/users/iman-akbar/repos")
+       .then(( response ) => {
+          console.log(response);
+            setAllDataList(response.data)
+        })
+    }
+    loadData();
+  }, [])
+
+  
+  return (      
+      <div>        
+      {allDataList.map((item,index) => {
           return (            
-            <div key={index} clas>              
-              <a className={styles.title} href={dataList.url}>{dataList.title}</a>
-              <p className={styles.url}>{dataList.url}</p>
+            <div key={index}>                
+              <a className={styles.title} href={item.html_url}>{item.name}</a>  
+              <p className={styles.url}>{item.html_url}</p>
             </div>
           );
         })}
       </div>
-    )
+    );
   }
-}
+
+
+export default Home
